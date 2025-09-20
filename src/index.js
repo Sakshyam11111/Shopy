4cde9f25e12773ffa0327f84a3a22c4351480924
Sakshyam11111 <sakshyamshrestha111@gmail.com>
2025-09-20 20:23:31 +0545

    new

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './Router'; // Adjust the path if necessary
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
